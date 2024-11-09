a0dce0578bddefe848e6a0f368d8a05ebbe0007b
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-09 14:54:01 +0530

    "Updated class name in Home component's div element from 'flex items-center justify-center h-screen bg-amber-100' to 'flex-center'"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-amber-100">
+    <div className="flex-center">
       <h1 className="h1">Storify - The only Storage solution you need</h1>
     </div>
   );
