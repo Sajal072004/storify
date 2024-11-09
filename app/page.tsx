1da14df827f4e7c98ed9332068c34cefa5cd58c0
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-09 14:59:04 +0530

    created auth pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="flex-center">
+    <div className="flex-center h-screen">
       <h1 className="h1">Storify - The only Storage solution you need</h1>
     </div>
   );
