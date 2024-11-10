56df3c84d33a0d60c9c1b40d8725f8793f09ee2b
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-11 01:39:26 +0530

    "Remove Home page component, update user.actions.ts to add getCurrentUser function and modify createAccount, update next.config.ts to add image remote patterns"

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "..";
+import { createAdminClient, createSessionClient } from "..";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/const";
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -56,7 +57,7 @@ export const createAccount = async ({
         fullName,
         email,
         avatar:
-          "https://png.pngtree.com/png-clipart/20210915/ourmid/pngtree-user-avatar-placeholder-png-image_3918418.jpg",
+          avatarPlaceholderUrl,
           accountId
       },
     );
@@ -86,4 +87,20 @@ export const verifySecret = async({accountId , password} : {accountId:string, pa
 
   
 
+}
+
+export const getCurrentUser = async () => {
+  const {databases, account} = await createSessionClient();
+
+  const result = await account.get();
+
+  const user = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("accountId", result.$id)]
+  
+  );
+
+  if(user.total <= 0) return null;
+  return parseStringify(user.documents[0]);
 }
