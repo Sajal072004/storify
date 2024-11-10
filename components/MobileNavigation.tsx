369e927cd89551a26290a9c80955aff51777bb00
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-11 01:46:52 +0530

    "Updated MobileNavigation component to include logo, header, and sheet navigation with trigger and content"

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { set } from "zod";
 
 const MobileNavigation = () => {
+  const [open ,setOpen] = useState(false);
+  const pathname = usePathname();
   return (
-    <div>MobileNavigation</div>
-  )
-}
+    <header className="mobile-header">
+      <div className="flex items-center gap-4">
+      <Image
+        src={"/logo1.png"}
+        alt="logo"
+        width={60}
+        height={60}
+        className="h-auto rounded-[30%]"
+      />
+      <h1 className="text-brand text-3xl font-semibold">Storify</h1>
+      </div>
+      
 
-export default MobileNavigation
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image src={"/assets/icons/menu.svg"} alt="Search" width={30} height={30} />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
+};
+
+export default MobileNavigation;
