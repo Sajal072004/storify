6ea844c5bd62992b75f7a0b955f2e7b71aa5737d
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-10 17:07:26 +0530

    "Added imports, functions, and logic to user.actions.ts, updated index.ts, and added parseStringify function to utils.ts"

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,65 @@
-'use server'
+"use server";
 
-const createAccount = async({fullName,email}: {fullName:string , email:string}) => {
+import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "..";
+import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
 
-}
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", email)]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: unknown) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("failed to send an OTP");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+
+      {
+        fullName,
+        email,
+        avatar:
+          "https://png.pngtree.com/png-clipart/20210915/ourmid/pngtree-user-avatar-placeholder-png-image_3918418.jpg",
+          accountId
+      },
+    );
+  }
+
+  return parseStringify({accountId});
+};
