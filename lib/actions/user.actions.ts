448cb10c8b4a206bc30de7044b158b13daad78ec
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-11-12 01:55:01 +0530

    "Added try-catch block to getCurrentUser function and wrapped createSessionClient call in try block"

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -91,7 +91,9 @@ export const verifySecret = async ({
 };
 
 export const getCurrentUser = async () => {
-  const { databases, account } = await createSessionClient();
+
+  try {
+    const { databases, account } = await createSessionClient();
 
   const result = await account.get();
 
@@ -103,6 +105,10 @@ export const getCurrentUser = async () => {
 
   if (user.total <= 0) return null;
   return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
+  
 };
 
 export const signOutUser = async () => {
